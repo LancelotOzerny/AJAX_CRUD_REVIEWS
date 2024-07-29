b9138cab7e4982689ea249a002a5e9aa61ab7875
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-07-29 20:13:28 +0300

    Дописал обработк данных запроса и реализовал вывод уведомлений пользователю

---
+++ assets/js/addReview.js
@@ -0,0 +1,54 @@
+$("#CreateReviewButton").click(() => {
+    $("#CreateReviewButton").addClass("disabled");
+
+    $.ajax({
+        url: '/create.php',
+        method: 'post',
+        data: {
+            'NAME' : $("#InputName").val(),
+            'EMAIL' : $("#InputEmail").val(),
+            'TEXT' : $("#InputMessage").val(),
+        },
+
+        success: (data) => {
+            arr = JSON.parse(data);
+
+            $("#EmailInputErrors").text(arr['ERRORS']['EMAIL']);
+            $("#NameInputErrors").text(arr['ERRORS']['NAME']);
+            $("#TextInputErrors").text(arr['ERRORS']['TEXT']);
+
+            container = $("<div>", {
+                class: 'toast show mt-1',
+            });
+
+            if (arr['RESULT'] !== "undefined")
+            {
+                if (arr['RESULT'] === true)
+                {
+                    $("#InputName").val('')
+                    $("#InputEmail").val('')
+                    $("#InputMessage").val('')
+
+                    container.append($("<div>", {
+                        class: 'toast-body alert-success',
+                        text: 'Комментарий успешно добавлен!',
+                    }));
+                }
+                else
+                {
+                    container.append($("<div>", {
+                        class: 'toast-body alert-danger',
+                        text: 'Комментарий не добавлен. Исправьте ошибки в форме!',
+                    }));
+                }
+            }
+
+            $("#messages").append(container);
+            setTimeout(() => {
+                container.remove();
+            }, 3000);
+
+            $("#CreateReviewButton").removeClass("disabled");
+        }
+    });
+});
