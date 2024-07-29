25e3f2fadac5a07b1a8b36df3b825d948fe5ba9d
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-07-29 15:17:13 +0300

    Объявил orm-таблицу комментариев

---
+++ classes/reviewtable.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace classes;
+
+class ReviewTable
+{
+    public static function Create(array $params)
+    {
+
+    }
+
+    public static function Read(array $filter)
+    {
+
+    }
+
+    public static function Update(int $id, array $params)
+    {
+
+    }
+
+    public static function Delete(int $id)
+    {
+
+    }
+}
