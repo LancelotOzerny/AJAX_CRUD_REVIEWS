1de4c8a975b1b5c1d6e3f5f842d344ddd93b33d6
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-07-29 13:55:49 +0300

    Добавил настройки БД

---
+++ classes/databasevonfig.php
@@ -0,0 +1,10 @@
+<?php
+namespace classes;
+
+class DatabaseConfig
+{
+    public static $host = 'localhost';
+    public static $dbName = 'petProjects';
+    public static $user = 'root';
+    public static $password = '';
+}
