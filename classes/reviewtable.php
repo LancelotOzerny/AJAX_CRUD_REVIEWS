a7c1cdaef80fab36a04751bef38c93267c5eac94
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-07-29 15:33:16 +0300

    Реализовал чтение комментариев из БД

--- classes/reviewtable.php
+++ classes/reviewtable.php
@@ -9,9 +9,46 @@ class ReviewTable
 
     }
 
-    public static function Read(array $filter)
+    public static function Read(array $filter = []) : array
     {
+        $sql = 'SELECT * FROM Reviews';
 
+        $params = [];
+        foreach ($filter as $key => $value)
+        {
+            $lower = mb_strtolower($key);
+            $params[] = "$lower = :$lower";
+        }
+
+        if (empty($params) === false)
+        {
+            $sql .= ' WHERE ';
+            $sql .= join(', ', $params);
+        }
+
+        $prepare = DataBase::connection()->prepare($sql);
+
+        foreach ($filter as $key => $value)
+        {
+            $lower = mb_strtolower($key);
+            $prepare->bindValue(":$lower", $value);
+        }
+
+        $prepare->execute();
+        $arList = [];
+
+        while (($row = $prepare->fetch()) !== false)
+        {
+            $arList[] = [
+                'ID' => $row['id'],
+                'NAME' => $row['name'],
+                'EMAIL' => $row['email'],
+                'TEXT' => $row['text'],
+                'DATE' => $row['date'],
+            ];
+        }
+
+        return $arList;
     }
 
     public static function Update(int $id, array $params)
