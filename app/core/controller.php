f797cf19a8e9f63280185ae4134613acba216189
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-07-31 03:01:32 +0300

    Объявил классы контроллера и вида

---
+++ app/core/controller.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Core;
+
+abstract class Controller
+{
+    private $model;
+
+    public function __construct()
+    {
+
+    }
+
+    public function action_index()
+    {
+
+    }
+}
---
+++ app/core/view.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Core;
+
+class View
+{
+    public static function Generate(string $view, string $template, array $data)
+    {
+
+    }
+}
