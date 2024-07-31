9d7e3b76fce9457b7218db592198609746eca979
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-07-31 03:36:34 +0300

    Вынес страницу в шаблон

--- app/templates/crud/header.php
+++ app/templates/crud/header.php
@@ -1,1 +1,18 @@
-<?php
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>CRUD комментариев</title>
+
+    <link rel="stylesheet" href="/app/templates/crud/assets/css/reset.css">
+    <link rel="stylesheet" href="/app/templates/crud/assets/css/bootstrap.css">
+
+    <script src="/app/templates/crud/assets/js/jQuery.js"></script>
+    <script src="/app/templates/crud/assets/js/bootstrap.js"></script>
+</head>
+<body>
+<div id="spinner" class="d-flex justify-content-center align-items-center" style="height: 100vh;">
+    <img src="/app/templates/crud/assets/img/spinner.gif">
+</div>
+
+<div id="messages" class="position-fixed bottom-0 end-0 p-3" style="z-index: 11"></div>
--- app/views/view_main.php
+++ app/views/view_main.php
@@ -1,22 +1,3 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <title>CRUD комментариев</title>
-
-    <link rel="stylesheet" href="app/templates/crud/assets/css/reset.css">
-    <link rel="stylesheet" href="app/templates/crud/assets/css/bootstrap.css">
-
-    <script src="app/templates/crud/assets/js/jQuery.js"></script>
-    <script src="app/templates/crud/assets/js/bootstrap.js"></script>
-</head>
-<body>
-<div id="spinner" class="d-flex justify-content-center align-items-center" style="height: 100vh;">
-    <img src="app/templates/crud/assets/img/spinner.gif">
-</div>
-
-<div id="messages" class="position-fixed bottom-0 end-0 p-3" style="z-index: 11"></div>
-
 <div class="container">
     <div class="row">
         <div class="col-12">
@@ -103,7 +84,5 @@
         UpdateTable();
     });
 </script>
-<script src="app/templates/crud/assets/js/updateTable.js"></script>
-<script src="app/templates/crud/assets/js/addReview.js"></script>
-</body>
-</html>
+<script src="/app/templates/crud/assets/js/updateTable.js"></script>
+<script src="/app/templates/crud/assets/js/addReview.js"></script>
